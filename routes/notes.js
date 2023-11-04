0825c7ba58c9889ae67cf7678d1a21f9721a473d
Jimmy Wilson <james.l.wilson364@gmail.com>
2023-11-04 13:15:18 -0500

    added modularized routes and helper files

---
+++ routes/notes.js
@@ -0,0 +1,41 @@
+// require things
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+notes.get('/', (req, res) => {
+    console.info(`${req.method} request received for feedback`);
+  
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+  notes.post('/', (req, res) => {
+    
+    console.info(`${req.method} request received to submit feedback`);
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && test) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        note_id: uuid(),
+      };
+  
+      readAndAppend(newFeedback, './db/feedback.json');
+  
+      const response = {
+        status: 'success',
+        body: newFeedback,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting feedback');
+    }
+  });
+
+  // need a delete
+
+  module.exports = notes;
--- server.js
+++ server.js
@@ -1,12 +1,35 @@
+// require the things that I will be using
 const express = require('express');
 const fs  = require('fs');
 const noteData = require('./db/db.json');
+const api = require('./routes/index.js');
 
+// set up the port for heroku or 3001
+// use app to invoke express
 const PORT = 3001;
-
 const app = express();
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
 app.use(express.static('public'));
+
+// get route for the main page
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// get route for the notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// localhost launch from terminal line
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
 
 // WHEN I open the Note Taker
 // THEN I am presented with a landing page with a link to a notes page
