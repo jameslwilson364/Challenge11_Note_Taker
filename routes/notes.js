718389f5587dc57779d032728fbc58da920ad0e5
Jimmy Wilson <james.l.wilson364@gmail.com>
2023-11-05 13:00:11 -0600

    reset complete and post snippet was recovered

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 // require things
 const notes = require('express').Router();
 const path = require('path');
+const fs = require('fs');
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
@@ -9,11 +10,11 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
-  notes.post('/', (req, res) => {
+  notes.post('/notes', (req, res) => {
     
     console.info(`${req.method} request received to submit feedback`);
     const { noteTitle, noteText } = req.body;
-  
+    
     // If all the required properties are present
     if (noteTitle && noteText) {
       // Variable for the object we will save
@@ -21,9 +22,18 @@ notes.get('/', (req, res) => {
         title,
         text,
         note_id: uuid(),
-      };
-  
-      readAndAppend(newFeedback, './db/db.json');
+    };
+    
+    noteData.push(newNote);
+    const noteDataString = JSON.stringify(reviews);
+        
+    fs.writeFile(`./db/db.json`, noteDataString, (err) =>
+      err
+      ? console.error(err)
+      : console.log(
+      `Review for ${newNote.title} has been written to JSON file`
+      )
+    );
   
       const response = {
         status: 'success',
@@ -33,8 +43,8 @@ notes.get('/', (req, res) => {
       res.json(response);
     } else {
       res.json('Error in posting feedback');
-    }
-  });
+      }
+    });
 
   // need a delete
 
